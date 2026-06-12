524712b0ea0349ed6e9e354a0fe6c30c377b4cea
Sakshi <240301370066@centurionuniv.in>
2026-06-12 12:17:22 +0530

    Fix CORS for Netlify

--- cors.php
+++ cors.php
@@ -1,5 +1,5 @@
 <?php
-// Allow ALL origins (for Netlify + ngrok)
+// Allow all origins (for Netlify)
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
